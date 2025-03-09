1afc345e35366513598f21856898cb505bb68eb8
Daniela <daniacr002@gmail.com>
2025-03-08 23:34:02 -0500

    first commit

---
+++ src/components/Phrase.jsx
@@ -0,0 +1,19 @@
+
+const Phrase = ({ phraseRandom }) => {
+  const { phrase, author } = phraseRandom;
+
+  return (
+    
+      <article className='article'>
+        <div className="galleta_text">
+        
+          <p><strong>✨ ¡Feliz Día sarita! ✨</strong></p>
+          <p>¡Sigue brillando y muchos exitos! 💜</p>
+        </div>
+      </article>
+    
+
+  )
+}
+
+export default Phrase
